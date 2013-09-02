a4fdd4d4540e9c0f25efd38595f4bbea2dc4f8b4
Brent Hoover <brent@thebuddhalodge.com>
2013-09-02 03:43:35 -0700

    Center and enlarge video based on viewport

--- server.py
+++ server.py
@@ -2,14 +2,26 @@
 
 import SimpleHTTPServer
 import SocketServer
+from walkdir import filtered_walk, dir_paths, all_paths, file_paths
+
+class ChromeCastServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+	def __init__(self, request, client_address, server):
+		print(request)
+		super(ChromeCastServerHandler, self).__init__(request, client_address, server)
+
+def index_podcasts():
+	files = file_paths(filtered_walk('podcasts', included_files=['*.mp3', '*.mp4'], ))
+	return files
 
 
-def main(host='localhost', port=4040):
+def main(host='localhost', port=4041):
 	Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 	httpd = SocketServer.TCPServer((host, port), Handler)
-	print "serving at port", port
+	#files = index_podcasts()
 	return httpd
 
 if __name__ == '__main__':
+
 	httpd = main()
 	httpd.serve_forever()
